f0d35b0f4ed3bdf6fc1998d5daafdf8c5bfe360d
mbernardes19 <bernardes.matheus@outlook.com>
2021-12-04 17:40:33 -0300

    Adds background to whole page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <router-view></router-view>
+  <div class="app__background" />
 </template>
 
 <script>
@@ -11,8 +12,9 @@ export default {
 
 <style lang="scss">
 body {
-  background-color: #ffffff;
+  background-color: #101013;
   margin: 0;
+  height: 90vh;
 }
 
 #app {
@@ -21,5 +23,31 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.app__background {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+  filter: opacity(.8) brightness(1.25);
+  background-image: url('https://images.unsplash.com/photo-1528819027803-5473f2bf7633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=2260&q=5');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  @media screen and (max-width: 768px) {
+    background-image: url('https://images.unsplash.com/photo-1528819027803-5473f2bf7633?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8&w=1000&q=70');
+  }
 }
 </style>
