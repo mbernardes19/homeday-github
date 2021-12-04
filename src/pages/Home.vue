e399e1bf2c8fd938f0fe90b03d0e2a4e200dc8d1
mbernardes19 <bernardes.matheus@outlook.com>
2021-12-04 11:38:00 -0300

    Adds pages + router

---
+++ src/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="main-content">
+    <slider :slides="slides"></slider>
+  </main>
+</template>
+
+<script>
+import Slider from '@/components/Slider.vue';
+import slides from '../slides'
+
+export default {
+  name: 'Home',
+  components: {
+    Slider
+  },
+  data() {
+    return {
+      slides,
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.main-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90vh;
+}
+</style>
---
+++ src/pages/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="intro">
+    <h1>Welcome!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<style lang="scss">
+</style>
